9262ec48e25d31a48dcc7beb5a4e13df74cdb1f9
Marcelo Augusto <marceloaugustooliveirasoares@gmail.com>
2024-03-10 19:31:01 -0300

    Commit

--- Frontend/src/assets/js/pages/CadastrarAgendamentoPage.js
+++ Frontend/src/assets/js/pages/CadastrarAgendamentoPage.js
@@ -1,24 +1,33 @@
 import {InputValidation,TYPEVALIDATION} from "../utils/InputValidation.js"
 import {ApiAgendamento} from "../api/ApiAgendamento.js"
+import {FormValidation} from "../utils/FormValidation.js"
+import {ButtonValidation} from "../utils/ButtonValidation.js"
 let day = document.querySelectorAll(".day")
 let year = document.getElementById("year")
 const inputNome = new InputValidation("nome","nomeError","nomeSucess",TYPEVALIDATION.NAME);
 const inputDia = new InputValidation("Dia","DiaError","DiaSucess",TYPEVALIDATION.DATE);
-let testeData = new Date();
-
+const inputHorario = new InputValidation("Horario", "HorarioError","HorarioSucess",TYPEVALIDATION.TIME)
+const btnSubmit = new ButtonValidation("btnSubmitAgendamento")
+const FormSubmit = new FormValidation("formAgendamentos",[inputNome,inputDia,inputHorario],btnSubmit)
+FormSubmit.onChanges();
+let testeData = new Date(2024,1,2);
+console.log(testeData)
 async function Initialize(){
     const apiAgendamentos = new ApiAgendamento();
     let agendamentos = await apiAgendamentos.getAgendamentos();
     day.forEach((item)=>{
         item.addEventListener("click",(e)=>{
-            console.log("Fui clickado")
+            let dayCalender = document.querySelectorAll(".day")
+            dayCalender.forEach((itemCurrent)=>{
+                itemCurrent.classList.remove("current-day-mark");
+            })
+            if(item.innerHTML !== ""){
+                item.classList.add("current-day-mark")
+            }
+
 
         })
-        agendamentos.map((agendamento)=>{
-            if(item.innerHTML == agendamento.Dia){
-                item.classList.add("block")
-            }
-        })
+
     })
 }
 
