819cb25a892942384b1829dd550b13fae2cb9e28
Marcelo Augusto <marceloaugustooliveirasoares@gmail.com>
2024-03-10 11:52:32 -0300

    Commit

--- Frontend/src/assets/js/pages/CadastrarUsuarioPage.js
+++ Frontend/src/assets/js/pages/CadastrarUsuarioPage.js
@@ -1,7 +1,7 @@
 import { InputValidation, TYPEVALIDATION } from "../utils/InputValidation.js"
 
 
-const inputNome = new InputValidation("nome","nomeError", "nomeSucess",TYPEVALIDATION.TEXT);
+const inputNome = new InputValidation("nome","nomeError", "nomeSucess",TYPEVALIDATION.NAME);
 const inputCPF = new InputValidation("CPF","CPFError", "CPFSucess", TYPEVALIDATION.CPF);
 inputCPF.onChangeInput();
 inputNome.onChangeInput();
