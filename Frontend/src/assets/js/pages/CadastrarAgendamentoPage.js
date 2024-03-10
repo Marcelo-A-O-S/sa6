4c8f1e184f8745c80babe6563d461b91e41b8095
Marcelo Augusto <marceloaugustooliveirasoares@gmail.com>
2024-03-10 20:12:10 -0300

    Commit

--- Frontend/src/assets/js/pages/CadastrarAgendamentoPage.js
+++ Frontend/src/assets/js/pages/CadastrarAgendamentoPage.js
@@ -2,19 +2,20 @@ import {InputValidation,TYPEVALIDATION} from "../utils/InputValidation.js"
 import {ApiAgendamento} from "../api/ApiAgendamento.js"
 import {FormValidation} from "../utils/FormValidation.js"
 import {ButtonValidation} from "../utils/ButtonValidation.js"
-let day = document.querySelectorAll(".day")
-let year = document.getElementById("year")
+
+let year = document.getElementById("year");
+let dateCurrent = document.querySelector(".current-day");
+let btnLastMonth = document.getElementById("pre-year");
+let btnNextMonth = document.getElementById("next-year");
 const inputNome = new InputValidation("nome","nomeError","nomeSucess",TYPEVALIDATION.NAME);
-const inputDia = new InputValidation("Dia","DiaError","DiaSucess",TYPEVALIDATION.DATE);
-const inputHorario = new InputValidation("Horario", "HorarioError","HorarioSucess",TYPEVALIDATION.TIME)
+const inputData = new InputValidation("Data","DataError","DataSucess",TYPEVALIDATION.DATE);
+const inputHorarioInicial = new InputValidation("HorarioInicial", "HorarioInicialError","HorarioInicialSucess",TYPEVALIDATION.TIME)
+const inputHorarioFinal = new InputValidation("HorarioFinal", "HorarioFinalError","HorarioFinalSucess",TYPEVALIDATION.TIME)
 const btnSubmit = new ButtonValidation("btnSubmitAgendamento")
-const FormSubmit = new FormValidation("formAgendamentos",[inputNome,inputDia,inputHorario],btnSubmit)
+const FormSubmit = new FormValidation("formAgendamentos",[inputNome,inputData,inputHorarioInicial,inputHorarioFinal],btnSubmit)
 FormSubmit.onChanges();
-let testeData = new Date(2024,1,2);
-console.log(testeData)
-async function Initialize(){
-    const apiAgendamentos = new ApiAgendamento();
-    let agendamentos = await apiAgendamentos.getAgendamentos();
+async function InsertOnClick(){
+    let day = document.querySelectorAll(".day");
     day.forEach((item)=>{
         item.addEventListener("click",(e)=>{
             let dayCalender = document.querySelectorAll(".day")
@@ -24,11 +25,21 @@ async function Initialize(){
             if(item.innerHTML !== ""){
                 item.classList.add("current-day-mark")
             }
-
-
         })
-
     })
+}
+btnLastMonth.addEventListener("click",(e)=>{
+    console.log("Mês anterior")
+    InsertOnClick()
+})
+btnNextMonth.addEventListener("click",(e)=>{
+    console.log("Proximo mês")
+    InsertOnClick()
+})
+
+
+async function Initialize(){
+    InsertOnClick()
 }
 
 document.addEventListener("DOMContentLoaded",Initialize)
