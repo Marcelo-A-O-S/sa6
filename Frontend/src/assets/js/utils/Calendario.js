28df44fed22bc2cd567ff1ba699dce0de8654458
henrique silva <henrique@gmail.com>
2024-03-13 22:36:08 -0300

    abroba

--- Frontend/src/assets/js/utils/Calendario.js
+++ Frontend/src/assets/js/utils/Calendario.js
@@ -1,69 +1,63 @@
 const isLeapYear = (year) => {
-    return (
-      (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)
-    );
-  };
+  return (
+    (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)
+  );
+};
 
-  const getFebDays = (year) => {
-    return isLeapYear(year) ? 29 : 28;
-  };
+const getFebDays = (year) => {
+  return isLeapYear(year) ? 29 : 28;
+};
 
-  let calendar = document.querySelector('.calendar');
-  const month_names = [
-    'Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'
+const selecionarDia = () => {
+  let dias = document.querySelectorAll('.calendar-days .day');
+  dias.forEach(day => {
+    day.addEventListener('click', function() {
+      dias.forEach(d => d.classList.remove('selected'));
+      this.classList.add('selected');
+    });
+  });
+};
+
+const generateCalendar = (month, year) => {
+  let calendar_days = document.querySelector('.calendar-days');
+  calendar_days.innerHTML = '';
+  let calendar_header_year = document.querySelector('#year');
+  let days_of_month = [
+    31, getFebDays(year), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31
   ];
 
-  let month_picker = document.querySelector('#month-picker');
-  const dayTextFormat = document.querySelector('.day-text-format');
-  const timeFormat = document.querySelector('.time-format');
-  const dateFormat = document.querySelector('.date-format');
+  let currentDate = new Date();
 
-  month_picker.onclick = () => {
-    month_list.classList.remove('hideonce');
-    month_list.classList.remove('hide');
-    month_list.classList.add('show');
-    dayTextFormat.classList.remove('showtime');
-    dayTextFormat.classList.add('hidetime');
-    timeFormat.classList.remove('showtime');
-    timeFormat.classList.add('hideTime');
-    dateFormat.classList.remove('showtime');
-    dateFormat.classList.add('hideTime');
-  };
+  const month_picker = document.querySelector('#month-picker'); // Mover para dentro da função generateCalendar()
 
-  const generateCalendar = (month, year) => {
-    let calendar_days = document.querySelector('.calendar-days');
-    calendar_days.innerHTML = '';
-    let calendar_header_year = document.querySelector('#year');
-    let days_of_month = [
-      31, getFebDays(year), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31
-    ];
+  month_picker.innerHTML = month_names[month];
 
-    let currentDate = new Date();
+  calendar_header_year.innerHTML = year;
 
-    month_picker.innerHTML = month_names[month];
+  let first_day = new Date(year, month);
 
-    calendar_header_year.innerHTML = year;
+  for (let i = 0; i <= days_of_month[month] + first_day.getDay() - 1; i++) {
+    let day = document.createElement('div');
+    day.classList.add('day'); // Adiciona a classe 'day'
 
-    let first_day = new Date(year, month);
+    if (i >= first_day.getDay()) {
+      day.innerHTML = i - first_day.getDay() + 1;
 
-    for (let i = 0; i <= days_of_month[month] + first_day.getDay() - 1; i++) {
-      let day = document.createElement('div');
-
-      if (i >= first_day.getDay()) {
-        day.innerHTML = i - first_day.getDay() + 1;
-
-        if (i - first_day.getDay() + 1 === currentDate.getDate() &&
-          year === currentDate.getFullYear() &&
-          month === currentDate.getMonth()
-        ) {
-          day.classList.add('current-date');
-        }
+      if (i - first_day.getDay() + 1 === currentDate.getDate() &&
+        year === currentDate.getFullYear() &&
+        month === currentDate.getMonth()
+      ) {
+        day.classList.add('current-date');
       }
-      calendar_days.appendChild(day);
     }
-  };
+    calendar_days.appendChild(day);
+  }
 
-  let month_list = calendar.querySelector('.month-list');
+  // Após gerar os dias do calendário, chame a função para selecionar o dia
+  selecionarDia();
+
+  const month_list = document.querySelector('.month-list'); // Mover para dentro da função generateCalendar()
+
   month_names.forEach((e, index) => {
     let month = document.createElement('div');
     month.innerHTML = `<div>${e}</div>`;
@@ -81,54 +75,45 @@ const isLeapYear = (year) => {
       dateFormat.classList.add('showtime');
     };
   });
+};
 
-  (function() {
-    month_list.classList.add('hideonce');
-  })();
+// Variáveis para o seletor de mês e outros elementos
+const month_names = [
+  'Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'
+];
+const dayTextFormat = document.querySelector('.day-text-format');
+const timeFormat = document.querySelector('.time-format');
+const dateFormat = document.querySelector('.date-format');
+const calendar = document.querySelector('.calendar');
+const currentMonth = { value: new Date().getMonth() };
+const currentYear = { value: new Date().getFullYear() };
 
-  document.querySelector('#pre-year').onclick = () => {
-    --currentYear.value;
-    generateCalendar(currentMonth.value, currentYear.value);
-  };
-  document.querySelector('#next-year').onclick = () => {
-    ++currentYear.value;
-    generateCalendar(currentMonth.value, currentYear.value);
-  };
+generateCalendar(currentMonth.value, currentYear.value);
 
-  let currentDate = new Date();
-  let currentMonth = {
-    value: currentDate.getMonth()
-  };
-  let currentYear = {
-    value: currentDate.getFullYear()
-  };
+// Evento de clique para o seletor de mês
+const month_picker = document.querySelector('#month-picker');
+const month_list = document.querySelector('.month-list');
+
+month_picker.onclick = () => {
+  month_list.classList.remove('hideonce');
+  month_list.classList.remove('hide');
+  month_list.classList.add('show');
+  dayTextFormat.classList.remove('showtime');
+  dayTextFormat.classList.add('hidetime');
+  timeFormat.classList.remove('showtime');
+  timeFormat.classList.add('hideTime');
+  dateFormat.classList.remove('showtime');
+  dateFormat.classList.add('hideTime');
+};
+
+// Eventos para navegar entre os anos
+document.querySelector('#pre-year').onclick = () => {
+  --currentYear.value;
   generateCalendar(currentMonth.value, currentYear.value);
+};
+document.querySelector('#next-year').onclick = () => {
+  ++currentYear.value;
+  generateCalendar(currentMonth.value, currentYear.value);
+};
 
-  const todayShowTime = document.querySelector('.time-format');
-  const todayShowDate = document.querySelector('.date-format');
 
-  const currShowDate = new Date();
-  const showCurrentDateOption = {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-    weekday: 'long'
-  };
-  const currentDateFormat = new Intl.DateTimeFormat(
-    'pt-BR', showCurrentDateOption
-  ).format(currShowDate);
-  todayShowDate.textContent = currentDateFormat;
-
-  setInterval(() => {
-    const timer = new Date();
-    const option = {
-      hour: 'numeric',
-      minute: 'numeric',
-      second: 'numeric'
-    };
-    const formatTimer = new Intl.DateTimeFormat(
-      'pt-BR', option
-    ).format(timer);
-    let time = `${timer.getHours().toString().padStart(2, '0')}:${timer.getMinutes().toString().padStart(2, '0')}:${timer.getSeconds().toString().padStart(2, '0')}`;
-    todayShowTime.textContent = formatTimer;
-  }, 1000);
